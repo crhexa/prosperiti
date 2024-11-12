4615702cc0e24a21e4c7c72c5b9f2d7178c297bc
EvanChan321 <evanlugia@gmail.com>
2024-11-11 19:00:26 -0500

    Addded node modules for later use

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1>Prosperiti</h1>
       <h2>AI Personal Planner Assisant</h2>
       <div className="card">
-        <form id="user-input" >
-            <input type="text"></input>
+        <form id="user-search-form" >
+            <input type="text" id="user-input-entry"></input>
         </form>
       </div>
-    </>
+    </div>
   )
 }
 
