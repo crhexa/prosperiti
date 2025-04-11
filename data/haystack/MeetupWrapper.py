5be9484df7c3de1f67b267d9b4f1887dae2b0b87
Elliot Niemann <124651877+ENiems@users.noreply.github.com>
2025-04-11 03:46:16 -0400

    Revert "Merge branch 'main' of https://github.com/crhexa/prosperiti"
    This reverts commit 08e1474e009a72f0f38de3c99864c522ad7af0af, reversing
    changes made to fb7060cdf73ce2d1e95b1bcb3da8214a6ac441c5.

--- data/haystack/MeetupWrapper.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-
-class EventRetriever:
-    def __init__(self):
-        load_dotenv()
-        self.meetupClient = None
-
-    def getEvents(self):
-        return []
